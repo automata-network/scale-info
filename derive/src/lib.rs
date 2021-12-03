c29eba9426c5d84a838cb7faa52c7ba3bb771b4a
RyuH1 <haichuan@ata.network>
2021-12-04 00:19:03 +0800

    Adding feature(bool_to_option) to crate attributes

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![feature(bool_to_option)]
+
 extern crate alloc;
 extern crate proc_macro;
 
--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![feature(min_const_generics)]
+
 #![cfg_attr(not(feature = "std"), no_std)]
 #![deny(missing_docs)]
 
